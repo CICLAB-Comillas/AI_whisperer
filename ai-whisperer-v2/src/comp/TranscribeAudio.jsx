d165774e448ea7193ad5ac317485e53c6dae4027
Marta Hervas <201807895@alu.comillas.edu>
2023-06-27 10:14:56 +0200

    Con acceso al Audio en  Transcribe

--- ai-whisperer-v2/src/comp/TranscribeAudio.jsx
+++ ai-whisperer-v2/src/comp/TranscribeAudio.jsx
@@ -1,38 +1,39 @@
 import PropTypes from "prop-types";
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 
 export const TranscribeAudio = ({ audioURL }) => {
   const [showTextbox, setShowTextbox] = useState(false);
 
-  const handleClick = () => {
+  const handleClick = async () => {
     setShowTextbox(true);
 
-    // try {
-    //   const response = await fetch(audioURL);
-    //   // Realiza aquí la lógica para acceder y trabajar con el archivo de audio
-    //   setText('Audio recibido');
-    // } catch (error) {
-    //   console.error('Error al acceder al archivo de audio: ', error);
-    // }
+    try {
+      const response = await fetch(audioURL);
+      // Realiza aquí la lógica para acceder y trabajar con el archivo de audio
+    } catch (error) {
+      console.error('Error al acceder al archivo de audio: ', error);
+    }
 
   };
-  g
 
-  TranscribeAudio.propTypes = {
-    audioURL: PropTypes.string.isRequired
-  };
 
   return (
     <div>
       <button onClick={handleClick}>Transcribe</button>
       <br />
-      {/* <textarea value={text} readOnly /> */}
-      <br />
-      {showTextbox && <textarea rows="6" cols="60" />}
-
+      {showTextbox && (
+        <>
+          <p>Yeeeei tenemos audio</p>
+          <textarea rows="6" cols="60" readOnly />}
+        </>
+      )}
     </div>
 
   );
 };
+
+TranscribeAudio.propTypes = {
+  audioURL: PropTypes.string.isRequired
+};
+
 
